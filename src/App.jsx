299a69573d9c474a657232b26aaf3d78bcf23d53
Radu Alin <alin-stefan.radu@expleogroup.com>
2025-02-11 16:59:02 +0200

    header-and-fetch

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import Search from "./components/Search";
+import { useEffect, useState } from "react";
+
+const API_BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = import.meta.env.API_BASE_URL;
+
+const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
+
+function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const fetchMovies = async () => {
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const response = await fetch(endpoint, API_OPTIONS);
+      if (!response.ok) {
+        const err = await response.json();
+        console.log(err);
+        throw new Error("Failed to fetch movies");
+      }
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(`Error fetching movies: ${error}`);
+      setErrorMessage("Error fetching movies. Please try again");
+    }
+  };
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+  return (
+    <main>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You&apos;ll Enjoy
+            Without the Hassle
+          </h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        </header>
+        <section className="all-movies">
+          <h2>All Movies</h2>
+          {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+        </section>
+      </div>
+    </main>
+  );
+}
+
+export default App;
